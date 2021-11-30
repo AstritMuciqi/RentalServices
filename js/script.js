65d82b013d0fa688b3dbe21e923e8f3e79aaa77a
Astrit Muçiqi <am49608@ubt-uni.net>
2021-11-30 23:32:16 +0100

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(function() {
+	$('#slideshow > div:gt(0)').hide();
+	setInterval(function() {
+		$('#slideshow > div:first')
+		.fadeOut(1000)
+		.next()
+		.fadeIn(1000)
+		.end()
+		.appendTo('#slideshow');
+	}, 3850);
+});
+
+(function($) {
+	'use strict';
+	$.fn.swapClass = function(remove, add) {
+		this.removeClass(remove).addClass(add);
+		return this;
+	};
+}(jQuery));
